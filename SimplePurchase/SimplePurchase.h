7238038e785d5c1167b8bda87fd2a6de62241e8f
Chad Hendry <chendry@chendry.org>
2013-07-21 15:31:52 -0500

    generated an xcode project

---
+++ SimplePurchase/SimplePurchase.h
@@ -0,0 +1,13 @@
+//
+//  SimplePurchase.h
+//  SimplePurchase
+//
+//  Created by Chad Hendry on 7/21/13.
+//  Copyright (c) 2013 Chad Hendry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimplePurchase : NSObject
+
+@end
---
+++ SimplePurchase/SimplePurchase.m
@@ -0,0 +1,13 @@
+//
+//  SimplePurchase.m
+//  SimplePurchase
+//
+//  Created by Chad Hendry on 7/21/13.
+//  Copyright (c) 2013 Chad Hendry. All rights reserved.
+//
+
+#import "SimplePurchase.h"
+
+@implementation SimplePurchase
+
+@end
